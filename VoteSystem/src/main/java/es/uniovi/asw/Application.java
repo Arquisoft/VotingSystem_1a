494ed680b44522c03dc6ee055f3127634850e692
Carlos Villa <uo236851@uniovi.es>
2016-04-10 12:52:55 +0200

    Añadidos componentes necesarios y página de login

--- VoteSystem/src/main/java/es/uniovi/asw/Application.java
+++ VoteSystem/src/main/java/es/uniovi/asw/Application.java
@@ -1,18 +1,59 @@
 package es.uniovi.asw;
 
-import org.springframework.boot.Banner;
+
+import javax.faces.webapp.FacesServlet;
+import javax.servlet.ServletContext;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.context.ServletContextAware;
 
+@Configuration
 @EnableAutoConfiguration
-@SpringBootApplication
-public class Application {
+@ComponentScan
+@Controller
+@EnableJpaRepositories
+public class Application extends SpringBootServletInitializer implements ServletContextAware {
 
-  public static void main(String[] args) {
-    SpringApplication app = new SpringApplication(Application.class);
-    app.setBannerMode(Banner.Mode.OFF);
-    app.run(args);
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+        
+
+    @Override
+    public void setServletContext(ServletContext servletContext) {
+        servletContext.setInitParameter("com.sun.faces.forceLoadConfiguration", Boolean.TRUE.toString());
+        servletContext.setInitParameter("primefaces.CLIENT_SIDE_VALIDATION", "true");
+        servletContext.setInitParameter("javax.faces.PROJECT_STAGE", "Development");
+        
+    }
+
+
+    @Bean
+    public ServletRegistrationBean servletRegistrationBean(ApplicationContext context) {
+        ServletRegistrationBean jsfServlet = new ServletRegistrationBean(new FacesServlet(), "*.xhtml");
+        return jsfServlet;
+    }
     
-  }
+    
+  /* @Bean
+    public WebMvcConfigurerAdapter forwardToIndex(){
+    	return new WebMvcConfigurerAdapter(){
+    		@Override
+    		public void addViewControllers(ViewControllerRegistry registry){
+    			registry.addRedirectViewController("/", "/login.xhtml");			
+    			//registry.addViewController("/").setViewName("redirect:/principal.xhtml");
+    		}		
+    	};
+    }*/
+    
+
 }
