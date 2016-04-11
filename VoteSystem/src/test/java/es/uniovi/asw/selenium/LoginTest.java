8a3188240067f015bd69cd3153ba6c2d46d9b880
Carlos Villa <uo236851@uniovi.es>
2016-04-11 20:31:31 +0200

    Bug test

--- VoteSystem/src/test/java/es/uniovi/asw/selenium/LoginTest.java
+++ VoteSystem/src/test/java/es/uniovi/asw/selenium/LoginTest.java
@@ -43,7 +43,7 @@ public class LoginTest {
 	  String saucePassword = System.getenv("SAUCE_ACCESS_KEY");
 	  
 	          if (sauceUser != null && saucePassword != null && !sauceUser.isEmpty() && !saucePassword.isEmpty()) {
-	              URL saucelabs = new URL("http://" + sauceUser + ":" + saucePassword + "@localhost:4445/wd/hub");
+	              saucelabs = new URL("http://" + sauceUser + ":" + saucePassword + "@localhost:4445/wd/hub");
 	  
 	              // https://saucelabs.com/docs/platforms
 	              DesiredCapabilities capabilities = DesiredCapabilities.firefox();
