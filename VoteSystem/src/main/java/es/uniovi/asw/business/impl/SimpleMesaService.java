13420da195ec95217ad02b0920b2c1ed88f7fefa
Carlos Villa <uo236851@uniovi.es>
2016-04-14 02:07:30 +0200

    [Mesa elect.] Solo usuarios destinados a la mesa pueden votar

--- VoteSystem/src/main/java/es/uniovi/asw/business/impl/SimpleMesaService.java
+++ VoteSystem/src/main/java/es/uniovi/asw/business/impl/SimpleMesaService.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import es.uniovi.asw.business.MesaService;
+import es.uniovi.asw.model.User;
 import es.uniovi.asw.model.Votante;
+import es.uniovi.asw.persistence.UserRepository;
 import es.uniovi.asw.persistence.VotantesRepository;
 
 @Component
@@ -12,6 +14,9 @@ public class SimpleMesaService implements MesaService {
 	
 	 @Autowired
 	 private VotantesRepository votanteRep;
+	 
+	 @Autowired
+	 private UserRepository userRep;
 		
 	@Override
 	public boolean comprobarUsuario(String dni) {
@@ -31,6 +36,12 @@ public class SimpleMesaService implements MesaService {
 		Votante votante = new Votante(dniUsuario);
 		votanteRep.save(votante);
 		
+	}
+	
+	public User getUsuario(String dni){
+		User usuario = userRep.findBynif(dni);
+		usuario.setPassword(null);
+		return usuario;
 	}
 
 }
--- VoteSystem/src/main/java/es/uniovi/asw/presentation/BeanMesa.java
+++ VoteSystem/src/main/java/es/uniovi/asw/presentation/BeanMesa.java
@@ -8,10 +8,12 @@ import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.jsf.FacesContextUtils;
 
 import es.uniovi.asw.business.impl.SimpleMesaService;
+import es.uniovi.asw.model.LugarVoto;
+import es.uniovi.asw.model.User;
 
-@ManagedBean(name="mesa")
+@ManagedBean(name = "mesa")
 public class BeanMesa {
-	
+
 	private String dniUsuario;
 
 	public String getDniUsuario() {
@@ -21,30 +23,44 @@ public class BeanMesa {
 	public void setDniUsuario(String dniUsuario) {
 		this.dniUsuario = dniUsuario;
 	}
-	
-	public String verifyUser(){
-		
-		WebApplicationContext ctx =  FacesContextUtils.getWebApplicationContext(FacesContext.getCurrentInstance());
-		SimpleMesaService mesa = ctx.getBean(SimpleMesaService.class);
-		
-		boolean yaVoto = mesa.comprobarUsuario(dniUsuario);
-		
-		if(!yaVoto){
-			mesa.marcarVotante(dniUsuario);
-			FacesContext context = FacesContext.getCurrentInstance();	     
-	        context.addMessage(null, new FacesMessage("Éxito",  "Se ha registrado el votante con éxito") );
-		}	
-		
-		else{
-			FacesContext context = FacesContext.getCurrentInstance();	     
-	        context.addMessage(null, new FacesMessage("Fallo",  "Este usuario ya ha votado") );
-			
+
+	public String verifyUser() {
+
+		WebApplicationContext ctx = FacesContextUtils.getWebApplicationContext(FacesContext.getCurrentInstance());
+		SimpleMesaService mesaService = ctx.getBean(SimpleMesaService.class);
+
+		boolean yaVoto = mesaService.comprobarUsuario(dniUsuario);
+
+		if (!yaVoto) {
+
+			FacesContext context = FacesContext.getCurrentInstance();
+
+			LugarVoto mesa = (LugarVoto) FacesContext.getCurrentInstance().getExternalContext().getSessionMap()
+					.get("LOGGEDIN_MESA");
+
+			User usuario = mesaService.getUsuario(dniUsuario);
+
+			// Comprobamos que el usuario esté destinado en esa mesa
+			if (usuario.getCodigoMesa() == mesa.getId()) {
+
+				mesaService.marcarVotante(dniUsuario);
+				context.addMessage(null, new FacesMessage("Éxito", "Se ha registrado el votante con éxito"));
+
+			} else {
+
+				context.addMessage(null, new FacesMessage("Fallo", "Este usuario no puede votar en esta mesa"));
+			}
 		}
-		
-		dniUsuario=null;
-		
+
+		else {
+			FacesContext context = FacesContext.getCurrentInstance();
+			context.addMessage(null, new FacesMessage("Fallo", "Este usuario ya ha votado"));
+
+		}
+
+		dniUsuario = null;
+
 		return null;
 	}
-	
 
 }
